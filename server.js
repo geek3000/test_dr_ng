25d336c2f65ae643049731912ca8ce6125c60f1d
geek3000 <58487765+geek3000@users.noreply.github.com>
2022-11-19 20:38:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require('express');
-const { join } = require('path')
+const path = require('path');
 const app = express();
-const port = process.env.PORT || 8002;
 
-app.get('**', (req, res) => {
-    res.sendFile(join(__dirname, 'build', 'index.html'))
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.use(express.static(join(__dirname, 'build')));
-
-app.listen(port, () => {
-    console.log('Server app listening on port ' + port);
-});
+app.listen(9000);
